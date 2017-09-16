e7184591207a2f8aa1be911ce5fec499e5d0afb7
Alp Eren Can <alperenc@gmail.com>
2017-09-16 21:37:39 +0300

    add CreateContact component that's displayed based on state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
   state = {
+    screen: 'create',
     contacts: []
   }
 
@@ -24,9 +26,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <ListContacts 
+        {this.state.screen === 'list' && (
+          <ListContacts 
           onDeleteContact={this.removeContact} 
           contacts={this.state.contacts} />
+        )}
+        {this.state.screen === 'create' && (
+          <CreateContact/>
+        )}
       </div>
     )
   }
